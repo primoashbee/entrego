e805cbb3e83d7c982debacd442c30a87c6605007
Ashbee Morgado <ashbee.morgado@icloud.com>
2023-11-12 15:28:17 +0800

    z

--- app/Models/UserPersonalAssessment.php
+++ app/Models/UserPersonalAssessment.php
@@ -177,14 +177,14 @@ class UserPersonalAssessment extends Model
             $min_statement = "Low scorers prefer solitude and quiet environments. They may find social interactions draining rather than stimulating and may feel more comfortable following others' leads rather than taking initiative. Their smaller circle of close friends reflects their preference for deeper, more intimate connections. They may struggle with social anxiety and find it challenging to meet new people.";
         }
         if($max_label =='Extraversion'){
-            $max_label = "High scorers are typically outgoing and sociable, enjoying being the center of attention and initiating conversations. They are energized by social interaction and thrive in settings where they can connect with others. Their wide social circle reflects their ability to easily form meaningful connections. However, their tendency to speak without fully considering the consequences can sometimes lead to misunderstandings.";
+            $max_statement = "High scorers are typically outgoing and sociable, enjoying being the center of attention and initiating conversations. They are energized by social interaction and thrive in settings where they can connect with others. Their wide social circle reflects their ability to easily form meaningful connections. However, their tendency to speak without fully considering the consequences can sometimes lead to misunderstandings.";
         }
         
         if($min_label =='Agreeableness'){
             $min_statement = "Low scorers tend to be more focused on their own interests and goals. They may prioritize self-interest over the needs of others and may be less inclined to offer help or support. They may express indifference or even hostility towards those they perceive as obstacles to their objectives. Their focus on self-interest can sometimes lead to conflict and a lack of empathy for others' perspectives.";
         }
         if($max_label =='Agreeableness'){
-            $max_label = "High scorers are typically polite, compassionate, and helpful. They have a great deal of interest in other people, care about others' well-being, and feel empathy and concern for others. They enjoy helping and contributing to the happiness of others and readily extend support to those in need. However, their focus on others' needs may sometimes lead them to neglect their own needs or prioritize others' interests over their own.";
+            $max_statement = "High scorers are typically polite, compassionate, and helpful. They have a great deal of interest in other people, care about others' well-being, and feel empathy and concern for others. They enjoy helping and contributing to the happiness of others and readily extend support to those in need. However, their focus on others' needs may sometimes lead them to neglect their own needs or prioritize others' interests over their own.";
         }
         
 
@@ -192,26 +192,26 @@ class UserPersonalAssessment extends Model
             $min_statement = "Low scorers may be more spontaneous and flexible. They may struggle with organization and prioritization, leading to disarray and missed deadlines. Their lack of attention to detail may result in errors and a lack of polish in their work. They may prefer to complete tasks quickly rather than meticulously, leading to subpar results.";
         }
         if($max_label =='Conscientiousness'){
-            $max_label = "High scorers are typically organized, detail-oriented, and reliable. They value planning, preparation, and attention to detail, ensuring that important tasks are completed promptly and efficiently. Their preference for structure and routine brings order and tidiness to their daily lives. They prioritize completing tasks to a high standard, ensuring that all aspects are thoroughly considered and executed. However, their focus on perfection may sometimes lead to procrastination or an inability to let go of minor flaws.";
+            $max_statement = "High scorers are typically organized, detail-oriented, and reliable. They value planning, preparation, and attention to detail, ensuring that important tasks are completed promptly and efficiently. Their preference for structure and routine brings order and tidiness to their daily lives. They prioritize completing tasks to a high standard, ensuring that all aspects are thoroughly considered and executed. However, their focus on perfection may sometimes lead to procrastination or an inability to let go of minor flaws.";
         }
 
         if($min_label =='Neuroticism'){
             $min_statement = "Low scorers exhibit emotional stability and resilience in the face of challenges. They remain calm and composed under pressure, able to manage their emotions effectively. Negative emotions are less frequent and intense, allowing them to maintain a positive outlook. They adapt well to changes and unexpected events, demonstrating flexibility and a willingness to embrace new experiences. Stressful situations are viewed as opportunities for growth rather than insurmountable obstacles, enabling them to recover quickly and move forward.";
         }
         if($max_label =='Neuroticism'){
-            $max_label = "High scorers are typically prone to negative emotions such as anxiety, worry, and sadness. They may overthink potential problems or negative outcomes, leading to rumination and emotional distress. Stressful situations can easily overwhelm them, causing intense reactions and emotional turmoil. Their mood swings and fluctuations in emotional well-being can significantly impact their daily lives. The impact of stressful events can be prolonged and debilitating, making it difficult for them to bounce back quickly.";
+            $max_statement = "High scorers are typically prone to negative emotions such as anxiety, worry, and sadness. They may overthink potential problems or negative outcomes, leading to rumination and emotional distress. Stressful situations can easily overwhelm them, causing intense reactions and emotional turmoil. Their mood swings and fluctuations in emotional well-being can significantly impact their daily lives. The impact of stressful events can be prolonged and debilitating, making it difficult for them to bounce back quickly.";
         }
 
         if($min_label =='Openness'){
             $min_statement = "Low scorers prefer familiarity and routine over novelty and change. They tend to think in concrete terms, focusing on practical applications rather than theoretical explorations. Their preference for tradition and conformity may lead them to resist new ideas or unconventional approaches. They may view abstract concepts with skepticism, seeking clear and tangible evidence before accepting new perspectives..";
         }
         if($max_label =='Openness'){
-            $max_label = "High scorers are typically curious, adventurous, and creative. They embrace new experiences, ideas, and perspectives. Their strong imagination and creative mindset fuel their interest in intellectual pursuits and abstract thinking. They value exploration and the pursuit of new possibilities, seeking to expand their horizons and broaden their understanding of the world. Their appreciation for diversity and open-mindedness fosters a willingness to consider alternative viewpoints and engage in meaningful dialogue.";
+            $max_statement = "High scorers are typically curious, adventurous, and creative. They embrace new experiences, ideas, and perspectives. Their strong imagination and creative mindset fuel their interest in intellectual pursuits and abstract thinking. They value exploration and the pursuit of new possibilities, seeking to expand their horizons and broaden their understanding of the world. Their appreciation for diversity and open-mindedness fosters a willingness to consider alternative viewpoints and engage in meaningful dialogue.";
         }
 
 
         $min['statement'] = $min_statement;
-        $max['statement'] = $max_label;
+        $max['statement'] = $max_statement;
         
         return [
             'min'=>$min, 'max'=>$max
