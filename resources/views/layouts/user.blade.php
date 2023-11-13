e3b0b20c1488270c5c8d58473dcc898d75f53ac5
Ashbee Morgado <ashbee.morgado@icloud.com>
2023-11-13 15:54:56 +0800

    remove popper js

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -48,7 +48,7 @@
     @include('components.fixed-plugins')
 
     <!--   Core JS Files   -->
-    <script src="{{ asset('js/core/popper.min.js') }}"></script>
+    {{-- <script src="{{ asset('js/core/popper.min.js') }}"></script> --}}
     <script src="{{ asset('js/core/bootstrap.min.js') }}"></script>
     <script src="{{ asset('js/plugins/perfect-scrollbar.min.js') }}"></script>
     <script src="{{ asset('js/plugins/smooth-scrollbar.min.js') }}"></script>
