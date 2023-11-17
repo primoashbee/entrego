d857a4273ab662dc68942a89141c4fa15b662b6c
Ashbee Morgado <ashbee.morgado@icloud.com>
2023-11-17 18:22:08 +0800

    add hamburger

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -16,7 +16,7 @@ data-scroll="true">
     </nav>
     <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
         <div class="ms-md-auto pe-md-3 d-flex align-items-center">
-{{-- 
+            {{-- 
             <div class="input-group input-group-outline">
                 <label class="form-label">Type here...</label>
                 <input type="text" class="form-control">
@@ -157,8 +157,18 @@ data-scroll="true">
 
                 </a>
             </li>
+            <li class="nav-item d-flex align-items-center d-block d-sm-none" style="margin-top:14px">
+                <a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
+                    <div class="sidenav-toggler-inner">
+                        <i class="sidenav-toggler-line"></i>
+                        <i class="sidenav-toggler-line"></i>
+                        <i class="sidenav-toggler-line"></i>
+                    </div>
+                </a>
+            </li>
 
         </ul>
     </div>
+    
 </div>
 </nav>
